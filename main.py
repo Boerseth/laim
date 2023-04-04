66425bd363ab43804c7bc0860a83b31cb27269d8
frode <frode.thorsen.borseth@softwarelab.no>
2023-04-04 17:06:21 +0200

    Add som semblance of tests

--- main.py
+++ main.py
@@ -4,6 +4,59 @@ import makemore
 from micrograd import MLNN, Value
 
 
+def value_tests():
+    # Value tests
+    operand_1 = Value(1.0)
+    for operand_2 in [int(1), float(1), complex(1, 1), Value(1)]:
+        for op in ["+", "-", "*", "/"]:
+            assert isinstance(eval(f"operand_1 {op} operand_2"), Value)
+            assert isinstance(eval(f"operand_2 {op} operand_1"), Value)
+    val = Value(2.0)
+    assert isinstance(val.pow(3), Value)
+    assert isinstance(val.tanh(), Value)
+
+    # Maths tests
+    a = Value(1.0)
+    b = Value(2.0)
+    c = Value(3.0)
+    d = Value(4.0)
+
+    ad = a * d
+    bc = b * c
+    ad_bc = ad - bc
+    ad_bc_tanh = ad_bc.tanh()
+    ad_bc_pow_2 = ad_bc.pow(2)
+    loss = ad_bc_tanh.square_norm()
+
+    for comparison, val, expected in [
+        ("==", ad, 4.0),
+        ("==", bc, 6.0),
+        ("==", ad_bc, -2.0),
+        ("==", ad_bc_pow_2, 4.0),
+        ("<", ad_bc_tanh, -0.9),
+    ]:
+        expression = f"{val.data} {comparison} {expected}"
+        assert eval(expression), (val, comparison, expected)
+
+    # Gradient tests
+    ad_bc_tanh.grad = 1.0
+    assert all(val.grad == 0.0 for val in [a, b, c, d, ad, bc, ad_bc, ad_bc_pow_2])
+    ad_bc_tanh.backward()
+    ad_bc_tanh
+    ad_bc
+
+
+def run_tests():
+    for check_runner in [value_tests]:
+        try:
+            check_runner()
+        except AssertionError as ae:
+            print("Encountered assertion-error:", ae)
+    print("All checks passed!")
+
+
+
+
 def show_output(output: list[Value]) -> None:
     print(" > ", [f"{y:.4f}" for y in output])
 
@@ -46,9 +99,12 @@ def ngram() -> None:
 
 
 def main(
+    tests: bool,
     micrograd: bool,
     makemore: bool,  # Think about argparse namespaces
 ):
+    if tests:
+        run_tests()
     if micrograd:
         train_nn()
     if makemore:
@@ -58,10 +114,12 @@ def main(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("gpt", description="Try out ML code")
+    parser.add_argument("--tests", action="store_true", default=False, help="Run tests")
     parser.add_argument("--micrograd", action="store_true", default=False, help="Train a simple NN")
     parser.add_argument("--makemore", action="store_true", default=False, help="Generate names by training on n-grams")
     args = parser.parse_args()
     main(
+        args.tests,
         args.micrograd,
         args.makemore,
     )
